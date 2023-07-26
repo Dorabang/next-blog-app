65ac269c92344ff289191c6e297d0ae9587c0390
Dorabang <psa1195@naver.com>
2023-07-26 14:55:32 +0900

    chore: install create-next-app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <section className='leading-8'>
+        <p>[Park Su-a Introduction]</p>
+      </section>
+    </div>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  expoerimental: { appDir: true },
+};
+
+module.exports = nextConfig;
